5dcf290fc921a4297bbb9b34444e2d5b26b2bf17
Trinh Tong <trinhtong@Zipcoders-MacBook-Pro-8.local>
2018-12-19 08:46:09 -0500

    Revert "firebase hosting"
    This reverts commit d34d9caf548c246ebd594e2c93c9233e379c2753.

--- src/config/firebase/db_config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyCg3-W1kCBcY4R1OFmBTaJVCfyiSgxpVtA",
-    authDomain: "react-flashcards-781d5.firebaseapp.com",
-    databaseURL: "https://react-flashcards-781d5.firebaseio.com",
-    projectId: "react-flashcards-781d5",
-    storageBucket: "react-flashcards-781d5.appspot.com",
-    messagingSenderId: "113145246639"
-}
